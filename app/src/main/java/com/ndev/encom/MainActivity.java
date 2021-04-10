b35596ce439e7d7351476a973542109534e41386
NashifAlam <nashifalamscar@gmail.com>
2021-04-10 22:47:12 +0530

    Build tabs

--- app/src/main/java/com/ndev/encom/MainActivity.java
+++ app/src/main/java/com/ndev/encom/MainActivity.java
@@ -34,14 +34,14 @@ public class MainActivity extends AppCompatActivity {
         viewPager.setAdapter(sectionsPagerAdapter);
         TabLayout tabs = binding.tabs;
         tabs.setupWithViewPager(viewPager);
-        FloatingActionButton fab = binding.fab;
-
+        //FloatingActionButton fab = binding.fab;
+/*
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Login from here to view personal messages", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
-        });
+        });*/
     }
 }
